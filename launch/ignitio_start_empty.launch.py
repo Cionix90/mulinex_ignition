e35e787db59ccd175a349ec25db253c2c4d7a258
Jacopo Cioni <cionix90@gmail.com>
2024-10-17 12:34:32 +0200

    add jazzy branch and simulation launch

--- launch/ignitio_start_empty.launch.py
+++ launch/ignitio_start_empty.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler
+from launch.actions import RegisterEventHandler,OpaqueFunction
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
@@ -10,12 +10,29 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # Launch Arguments
+    
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    world_name = LaunchConfiguration('world',default="-r empty.sdf")
     #Declare Launch
+
+
+
     use_sim_time_arg = DeclareLaunchArgument(
             'use_sim_time',
             default_value=use_sim_time,
             description='If true, use simulated clock')
+    world_name_arg = DeclareLaunchArgument(
+            'world',
+            default_value=world_name,
+            description='If true, use simulated clock')
+
+    #get world name
+
+    world = PathJoinSubstitution(
+                [FindPackageShare('gazebo_models_worlds_collection'),
+                 'worlds', world_name]
+            )
+
     # Get URDF via xacro
     robot_description_content = Command(
         [
@@ -73,7 +90,9 @@ def generate_launch_description():
                 [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
                                        'launch',
                                        'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]),
+                launch_arguments={
+                    "gz_args": world_name
+                }.items()),
         # RegisterEventHandler(
         #     event_handler=OnProcessExit(
         #         target_action=gz_spawn_entity,
@@ -83,6 +102,7 @@ def generate_launch_description():
         node_robot_state_publisher,
         gz_spawn_entity,
         # Launch Arguments
-        use_sim_time_arg
+        use_sim_time_arg,
+        world_name_arg
         
     ])
